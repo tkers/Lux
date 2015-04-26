88f74e6bf28161aeaa537094689e9cf12655a465
Tijn Kersjes <tijn@divbyzero.nl>
2015-04-27 00:19:32 +0200

    TOTP available as a class

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function spinCode(elem, newCode) {
 
 	var spinner = window.setInterval(function () {
-		elem.innerHTML = Math.floor(Math.random()*900000) + 100000;
+		elem.innerHTML = Math.floor(Math.random() * 900000) + 100000;
 	}, 25);
 	
 	window.setTimeout(function () {
@@ -12,24 +12,26 @@ function spinCode(elem, newCode) {
 
 window.onload = function () {
 
-var serviceName = document.getElementById("servicename");
-var serviceCode = document.getElementById("servicecode");
-var serviceList = document.getElementById("servicelist");
-var serviceTimer = document.getElementById("servicetimer");
+    var serviceName = document.getElementById("servicename");
+    var serviceCode = document.getElementById("servicecode");
+    var serviceList = document.getElementById("servicelist");
+    var serviceTimer = document.getElementById("servicetimer");
 
-var lastExpire = -1;
+    var lastExpire = -1;
 
-window.setInterval(function () {
-	
-	var expire = getExpiry();
-	if (expire > lastExpire) {
-		var token = generateTOTP("twzunjmhli73wsv2");
-		serviceName.innerHTML = "GitHub";
-		spinCode(serviceCode, token);
-	}
-	lastExpire = expire;
-	var timerWidth = 100 * expire / 30;
-	serviceTimer.style.width = timerWidth + "%";
-}, 10);
+    var totp = new TOTP("twzunjmhli73wsv2");
 
-}
+    window.setInterval(function () {
+
+        var expire = totp.getTTL();
+        if (expire > lastExpire) {
+            var token = totp.getToken();
+            serviceName.innerHTML = "GitHub";
+            spinCode(serviceCode, token);
+        }
+        lastExpire = expire;
+        var timerWidth = 100 * expire / 30;
+        serviceTimer.style.width = timerWidth + "%";
+    }, 10);
+
+};
--- totp.js
+++ totp.js
@@ -1,125 +1,134 @@
-function getExpiry() {
+var TOTP = (function () {
 
-	var stepSize = 30;
+    function TOTP(key, options) {
 
-	var seconds = new Date().getTime() / 1000;
-	var sinceStep = seconds % stepSize;
-	
-	// return seconds left in current timestep
-	return stepSize - sinceStep;
-}
+        if (!key) throw new Error("No key provided");
+        if (!options) options = {};
 
-function generateTOTP(key) {
-
-	var tokenLength = 6;
-	var timeStep = 30;
-	
-	// get the current timestep
-	var time = new Date().getTime() / 1000;	
-	var timeCounter = Math.floor(time / timeStep);
-	
-	// calculate token value
-	var value = generateHOTP(key, timeCounter);
-	
-	// only use 6 last digits
-	var token = value % (Math.pow(10, tokenLength));
-	
-	// pad with zeroes
-	token = zeroPad(token, tokenLength);
-	
-	// all done!
-	return token;
-}
-
-function generateHOTP(key, counter) {
-
-	// convert key and counter to bytes
-	counter = decToBytes(counter, 8);
-	key = b32ToBytes(key);
-
-	// calculate HMAC value
-	var hash = Crypto.HMAC(Crypto.SHA1, counter, key);
-	var bytes = hexToBytes(hash);
-	
-	// truncate hash
-	var off = bytes[19] & 0xf;
-	return (bytes[off]     & 0x7f) << 24 |
-           (bytes[off + 1] & 0xff) << 16 |
-           (bytes[off + 2] & 0xff) << 8  |
-           (bytes[off + 3] & 0xff);
-}
-
-function decToBytes(x, n) {
-
-	var bytes = [];
-	while (x > 0) {
-		bytes.unshift(x & 255);
-		x >>= 8;
-	}
-
-	zeroPadArray(bytes, n);
-
-	return bytes;
-}
-
-function b32ToBytes(x, n) {
-
-    var bits = b32ToBits(x);
-    return bitsToBytes(bits, n);
-}
-
-function hexToBytes(x, n) {
-
-	var bytes = [];
-	for(var i = 0; i < x.length; i += 2) {
-        var byte = parseInt(x.substr(i, 2), 16);
-		bytes.push(byte);
-	}
-
-    zeroPadArray(bytes, n);
-
-	return bytes;
-}	
-
-function b32ToBits(x) {
-
-    var dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-    var bits = "";
-    for (var i = 0; i < x.length; i++) {
-        var value = dict.indexOf(x.charAt(i).toUpperCase());
-        bits += zeroPad(value.toString(2), 5);
+        this._key = key;
+        this._size = options.size || 6;
+        this._interval = options.interval || 30;
     }
 
-    return bits;
-}
+    TOTP.prototype.getToken = function (date) {
 
-function bitsToBytes(x, n) {
+        // defaults to getting current token
+        if (!date) date = new Date();
 
-    var bytes = [];
-    for(var i = 0; i < x.length; i += 8) {
-        var byte = parseInt(x.substr(i, 8), 2);
-        bytes.push(byte);
+        // get the current timestep
+        var time = date.getTime() / 1000;
+        var timeStep = Math.floor(time / this._interval);
+
+        // calculate token value
+        var value = generateHOTP(this._key, timeStep);
+
+        // only use 6 last digits
+        var token = value % (Math.pow(10, this._size));
+
+        // pad with zeroes
+        token = zeroPad(token, this._size);
+
+        // all done!
+        return token;
+    };
+
+    TOTP.prototype.getTTL = function () {
+
+        var now = new Date().getTime() / 1000;
+        var age = now % this._interval;
+
+        // return seconds left
+        return this._interval - age;
+    };
+
+    function generateHOTP(key, counter) {
+
+    	// convert key and counter to bytes
+    	counter = decToBytes(counter, 8);
+    	key = b32ToBytes(key);
+
+    	// calculate HMAC value
+    	var hash = Crypto.HMAC(Crypto.SHA1, counter, key);
+    	var bytes = hexToBytes(hash);
+
+    	// truncate hash
+    	var off = bytes[19] & 0xf;
+    	return (bytes[off]     & 0x7f) << 24 |
+               (bytes[off + 1] & 0xff) << 16 |
+               (bytes[off + 2] & 0xff) << 8  |
+               (bytes[off + 3] & 0xff);
     }
 
-    zeroPadArray(bytes, n);
+    function decToBytes(x, n) {
 
-    return bytes;
-}
+    	var bytes = [];
+    	while (x > 0) {
+    		bytes.unshift(x & 255);
+    		x >>= 8;
+    	}
 
-function zeroPad(x, n) {
+    	zeroPadArray(bytes, n);
 
-	x = x.toString();
-	while (x.length < n) {
-		x = "0" + x;
-	}
-
-	return x;
-}
-
-function zeroPadArray(a, n) {
-
-    while (a.length < n) {
-        a.unshift(0);
+    	return bytes;
     }
-}
+
+    function b32ToBytes(x) {
+
+        var bits = b32ToBits(x);
+        return bitsToBytes(bits);
+    }
+
+    function hexToBytes(x) {
+
+    	var bytes = [];
+    	for(var i = 0; i < x.length; i += 2) {
+            var byte = parseInt(x.substr(i, 2), 16);
+    		bytes.push(byte);
+    	}
+
+    	return bytes;
+    }
+
+    function b32ToBits(x) {
+
+        var dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+        var bits = "";
+        for (var i = 0; i < x.length; i++) {
+            var value = dict.indexOf(x.charAt(i).toUpperCase());
+            bits += zeroPad(value.toString(2), 5);
+        }
+
+        return bits;
+    }
+
+    function bitsToBytes(x) {
+
+        var bytes = [];
+        for(var i = 0; i < x.length; i += 8) {
+            var byte = parseInt(x.substr(i, 8), 2);
+            bytes.push(byte);
+        }
+
+        return bytes;
+    }
+
+    function zeroPad(x, n) {
+
+    	x = x.toString();
+    	while (x.length < n) {
+    		x = "0" + x;
+    	}
+
+    	return x;
+    }
+
+    function zeroPadArray(a, n) {
+
+        while (a.length < n) {
+            a.unshift(0);
+        }
+    }
+
+    return TOTP;
+})();
